f12fdf66367681deb1f86f918cc73fe57ae53373
Dima Dovgal <dovgal.dima.86@gmail.com>
2023-01-12 17:30:34 +0200

    Add new commit to project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
 'use strict';
 
-let formSearch, inputSearch, btnSearch, openSearch;
+let formSearch, inputSearch, openSearch;
 
 formSearch = document.querySelector('#form_search');
 inputSearch = document.querySelector('#input_search');
-btnSearch = document.querySelector('#btn_search');
 
 formSearch.addEventListener('submit', (e) => {
     e.preventDefault();
 
     openSearch = `https://www.google.com/search?q=${inputSearch.value}&sitesearch=https://dovgaldima.pp.ua`;
+    
     if(!inputSearch) {
         inputSearch.placeholder = 'Заповніть поле пошуку!';
         return false;
     } else {
         window.open(openSearch);
-    }
+    };
+
     formSearch.reset();
 })
-
-console.log(inputSearch.value)
